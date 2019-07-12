3d51a3050b74ba12bfd34074e06c9e34c10fe7fe
terry <terry@feekr.com>
2019-07-12 13:36:52 +0800

    cansole

--- webhook.go
+++ webhook.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/Unknwon/goconfig"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
-	"github.com/Unknwon/goconfig"
 )
-
-//X-Gitlab-Token header 的注释 ？
 
 func index(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "传参需要header：%v和get请求http://localhost:10086/?program=%v","X-Gitlab-Token","项目名")
+	fmt.Fprintf(w, "传参需要header：%v和get请求http://localhost:10086/?program=%v", "X-Gitlab-Token", "项目名")
 
 	cfg, _ := goconfig.LoadConfigFile("conf.ini")
 	Token, _ := cfg.GetValue("config", "token")
@@ -24,6 +22,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		log.Println("Header param 'X-Gitlab-Token' is missing")
 		return
 	}
+
 	token := string(tokens[0])
 	if !strings.EqualFold(token, Token) {
 		log.Println("Header param 'X-Gitlab-Token' is error")
@@ -49,7 +48,6 @@ func execShell(program string) {
 		fmt.Printf("Shell file %v is not exists", command)
 		return
 	}
-
 	cmd := exec.Command("/bin/bash", "-c", command)
 
 	output, err := cmd.Output()
@@ -62,10 +60,9 @@ func execShell(program string) {
 }
 
 func main() {
-	// 设置路由，如果访问/，则调用index方法
+
 	http.HandleFunc("/", index)
 
-	// 启动web服务，监听9090端口
 	err := http.ListenAndServe(":10086", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
